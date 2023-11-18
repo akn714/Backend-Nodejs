b5d3db6341052d2a5c64187e90eb02890e5bca99
adarshkumar714 <adarshkumar20012704@gmail.com>
2023-11-19 00:09:49 +0530

    added one left route of top3plans

--- app/routers/planRouter.js
+++ app/routers/planRouter.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const { protectRoute, isAuthorised } = require('../controller/authController')
 const planRouter = express.Router()
-const {getPlan, getAllPlans, createPlan, updatePlan, deletePlan} = require('../controller/planController')
+const {getPlan, getAllPlans, createPlan, updatePlan, deletePlan, top3Plans} = require('../controller/planController')
 
 planRouter.route('/allPlans')
 .get(getAllPlans)
 
 // top 3 plans
+planRouter.route('/top3plans')
+.get(top3Plans)
 
 planRouter.use(protectRoute)
 planRouter.route('/:id')
